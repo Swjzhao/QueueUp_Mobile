8d0111634a35f5e0fc962540318434551bfe3211
Steven Zhao <stevenwjzhao@hotmail.com>
2019-07-16 22:03:39 -0400

    Added auth and chat

---
+++ lib/Auth.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  static const String id = "CREATEACCOUNT";
+  @override
+  _CreateAccountState createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Login extends StatefulWidget {
+  static const String id = "LOGIN";
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Auth.dart';
+import 'Chat.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,13 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
+      theme: ThemeData.dark(),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  static const String id = "HOMESCREEN";
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -30,14 +33,49 @@ class MyHomePage extends StatelessWidget {
                 child: Container(
                 ),
 
-              )
+              ),
+              Text(
+                "QueueUp",
+                style: TextStyle(fontSize: 40.0),
+              ),
             ],
           ),
-          Text(
-            "QueueUp",
-            style: TextStyle(fontSize: 40.0),
-          )
+          SizedBox(
+            height: 50.0,
+          ),
+          CustomButton(
+            text:"Log In",
+            callback:(){},
+          ),
+          CustomButton(
+            text:"Register",
+            callback:(){},
+          ),
         ],
+      )
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback callback;
+  final String text;
+
+  const CustomButton({Key key, this.callback, this.text}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child:Material(
+          color: Colors.blueGrey,
+        elevation:6.0,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: callback,
+          minWidth: 200.0,
+          height: 45.0,
+          child: Text(text),
+        )
       )
     );
   }
