ba162300c517cb5f2ba5f9f4d1c77acf17ea6ea8
Steven Zhao <stevenwjzhao@hotmail.com>
2019-07-18 23:49:36 -0400

    Added firebase auth

--- lib/Auth.dart
+++ lib/Auth.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'Chat.dart';
 
 class CreateAccount extends StatefulWidget {
   static const String id = "CREATEACCOUNT";
@@ -7,9 +10,39 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> registerUser() async {
+    FirebaseUser user = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password);
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Chat()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tensor Chat"),
+      ),
+
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Hero(
+              tag:'logo',
+              child: Container(
+              ),
+            ),
+          )
+        ],
+      )
+    );
   }
 }
 
@@ -22,7 +55,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
 
--- lib/Chat.dart
+++ lib/Chat.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'Auth.dart';
 
 class Chat extends StatefulWidget {
   static const String id = "CHAT";
