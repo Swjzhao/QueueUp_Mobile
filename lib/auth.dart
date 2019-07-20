f58dbe33e4aa02462f07d7a825316255bf103066
Steven Zhao <stevenwjzhao@hotmail.com>
2019-07-19 23:38:15 -0400

    Added a bunch of stuff

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'Chat.dart';
-import 'Util.dart';
+import 'chat_main.dart';
+import 'util.dart';
 
 class CreateAccount extends StatefulWidget {
   static const String id = "CREATEACCOUNT";
@@ -20,6 +20,86 @@ class _CreateAccountState extends State<CreateAccount> {
     FirebaseUser user = await _auth.createUserWithEmailAndPassword(
         email: email, password: password);
     Navigator.push(context, MaterialPageRoute(builder: (context) => Chat()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QueueUp"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Hero(
+              tag: 'logo',
+              child: Container(
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => email = value,
+            decoration: InputDecoration(
+              hintText: "Enter Your Email...",
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            autocorrect: false,
+            obscureText: true,
+            onChanged: (value) => password = value,
+            decoration: InputDecoration(
+              hintText: "Enter Your Password...",
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          CustomButton(
+            text: "Create An Account",
+            callback: () async {
+              await registerUser();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  static const String id = "LOGIN";
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> loginUser() async {
+    FirebaseUser user = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Chat(
+          user: user,
+        ),
+      ),
+    );
   }
 
   @override
@@ -66,26 +146,13 @@ class _CreateAccountState extends State<CreateAccount> {
             ),
           ),
           CustomButton(
-            text: "Register",
+            text: "Log In",
             callback: () async {
-              await registerUser();
+              await loginUser();
             },
           )
         ],
       ),
     );
-  }
-}
-
-class Login extends StatefulWidget {
-  static const String id = "LOGIN";
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'Auth.dart';
-import 'Chat.dart';
-import 'Util.dart';
+import 'auth.dart';
+import 'chat_main.dart';
+import 'util.dart';
 
 void main() => runApp(MyApp());
 
